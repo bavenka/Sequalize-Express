a0befd78c73d8fffe513f67c679b6e5a8e7b3841
Pavel_Kavalchuk <bavenka@gmail.com>
2018-04-17 17:36:30 +0300

    supplemented category validator

--- src/models/Category.js
+++ src/models/Category.js
@@ -2,17 +2,22 @@ import Product from './Product';
 import connect from '../database/connect';
 
 const {
-    sequelize,
-    Sequelize
+  sequelize,
+  Sequelize
 } = connect;
 
 const Category = sequelize.define('category', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-    },
-}, { timestamps: false });   
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  order: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+}, {timestamps: false});
 
 Category.hasMany(Product, {as: 'Products'})
 Product.belongsTo(Category);
